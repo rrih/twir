eb3b5842e664e5c4189db58c96ca8d335d0a94d3
rrih <kawahara.ryohei@lancers.co.jp>
2021-01-25 00:25:10 +0900

    :+1: fix

--- utils/url.ts
+++ utils/url.ts
@@ -1,3 +1,2 @@
 export const api: string = process.env.NODE_ENV === 'production' ? 'https://twir.vercel.app' : 'http://localhost:3000'
-export const url: string =
-	process.env.NODE_ENV === 'production' ? 'https://rrih.github.io/twir' : 'http://localhost:3000'
+export const url: string = process.env.NODE_ENV === 'production' ? 'https://rrih.github.io/twir' : 'http://localhost:3000'
